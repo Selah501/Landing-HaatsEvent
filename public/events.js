f8bbd380c745230cc692290bf6f5ff05217a1e4b
selah501 <danbyfa@gmail.com>
2026-07-06 23:28:06 +0900

    Update event list and UI, add basic vent product

--- public/events.js
+++ public/events.js
@@ -1,28 +1,42 @@
 const eventsData = [
     {
-        id: "ev_lotte",
-        title: "내포 롯데캐슬 핫썸머",
-        priceText: "하츠 복합환풍기 티오람미니 파격세일 195,000원",
-        periodText: "접수기간: 2026. 06. 30(화) ~ 07. 06(월)",
-        status: "active"
+        "id": "ev_1783340698960",
+        "title": "전원주택 빌라 핫썸머",
+        "priceText": "하츠 복합환풍기 티오람미니 파격세일 195,000원",
+        "periodText": "접수기간: 2026. 07. 07(화) ~ 07. 20(월)",
+        "status": "active"
     },
     {
-        id: "ev_moa",
-        title: "내포 모아엘가 핫썸머",
-        priceText: "하츠 복합환풍기 티오람미니 파격세일 195,000원",
-        periodText: "접수기간: 2026. 06. 30(화) ~ 07. 06(월)",
-        status: "active"
+        "id": "ev_1783340698959",
+        "title": "내포 LH스타힐스 핫썸머",
+        "priceText": "하츠 복합환풍기 파격세일 195,000원",
+        "periodText": "접수기간: 2026. 07. 06(월) ~ 07. 20(월)",
+        "status": "active"
     },
     {
-        id: "ev_eg1",
-        title: "내포 이지더원 1차 핫썸머 2026.6",
-        periodText: "~6/29",
-        status: "ended"
+        "id": "ev_lotte",
+        "title": "내포 롯데캐슬 핫썸머",
+        "priceText": "하츠 복합환풍기 티오람미니 파격세일 195,000원",
+        "periodText": "접수기간: 2026. 06. 30(화) ~ 07. 06(월)",
+        "status": "active"
     },
     {
-        id: "ev_eg2",
-        title: "내포 이지더원 2차 핫썸머",
-        periodText: "~6/30",
-        status: "ended"
+        "id": "ev_moa",
+        "title": "내포 모아엘가 핫썸머",
+        "priceText": "하츠 복합환풍기 티오람미니 파격세일 195,000원",
+        "periodText": "접수기간: 2026. 06. 30(화) ~ 07. 06(월)",
+        "status": "active"
+    },
+    {
+        "id": "ev_eg1",
+        "title": "내포 이지더원 1차 핫썸머 2026.6",
+        "periodText": "~6/29",
+        "status": "ended"
+    },
+    {
+        "id": "ev_eg2",
+        "title": "내포 이지더원 2차 핫썸머",
+        "periodText": "~6/30",
+        "status": "ended"
     }
 ];

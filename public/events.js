9c05cafaf04db9417738c9ee8482c5b11162476d
selah501 <danbyfa@gmail.com>
2026-07-16 02:51:04 +0900

    feat: add batch assign modal and update products

--- public/events.js
+++ public/events.js
@@ -1,65 +1,64 @@
 const globalProducts = [
     {
-        id: 'p_tioram',
-        name: '복합환풍기',
-        model: '티오람 미니',
-        features: '배기/냄새차단/제습/온풍/자연풍',
-        cost: '',
-        defaultPriceText: '195,000원',
-        detailLink: './products/tioram-mini.html',
-        remarks: ''
+        "id": "p_tioram",
+        "name": "복합환풍기",
+        "model": "티오람 미니",
+        "features": "배기/냄새차단/제습/온풍/자연풍",
+        "cost": "",
+        "defaultPriceText": "195,000원",
+        "detailLink": "./products/tioram-mini.html",
+        "remarks": ""
     },
     {
-        id: 'p_basic',
-        name: '일반 환풍기',
-        model: '냄새차단 전동댐퍼 일체형',
-        features: '단순 교체 / 담배냄새 완벽 차단',
-        cost: '',
-        defaultPriceText: '12만~14만',
-        detailLink: './products/basic-vent.html',
-        remarks: ''
+        "id": "p_terra",
+        "name": "테라 주방 후드",
+        "model": "DTR-90-MSHZ",
+        "features": "주방 공기를 쾌적하게",
+        "cost": "",
+        "defaultPriceText": "350,000원",
+        "detailLink": "./products/hood.html",
+        "remarks": ""
     },
     {
-        id: 'p_terra',
-        name: '하츠 명품 후드',
-        model: '테라 DTR-90-MSHZ',
-        features: '주방 공기를 쾌적하게',
-        cost: '',
-        defaultPriceText: '350,000원',
-        detailLink: './products/hood.html',
-        remarks: ''
+        "id": "p_hurricane",
+        "name": "허리케인 주방 후드",
+        "model": "HRH90S",
+        "features": "강력한 흡입력",
+        "cost": "",
+        "defaultPriceText": "350,000원",
+        "detailLink": "./products/hood.html",
+        "remarks": ""
     },
     {
-        id: 'p_hurricane',
-        name: '하츠 명품 후드',
-        model: '허리케인 HRH90S',
-        features: '강력한 흡입력',
-        cost: '',
-        defaultPriceText: '350,000원',
-        detailLink: './products/hood.html',
-        remarks: ''
+        "id": "p_sink",
+        "name": "사각씽크볼 폰테 세트",
+        "model": "세트 HZ-EW8502SF",
+        "features": "올스텐 배수구, 폰테206 4웨이",
+        "cost": "",
+        "defaultPriceText": "550,000원",
+        "detailLink": "./products/sink-faucet.html",
+        "remarks": ""
     },
     {
-        id: 'p_sink',
-        name: '사각씽크볼 & 폭포수전',
-        model: '프리미엄 세트',
-        features: '주방의 품격을 높이는 디자인',
-        cost: '',
-        defaultPriceText: '별도 문의',
-        detailLink: './products/sink-faucet.html',
-        remarks: ''
+        "id": "p_flex",
+        "name": "일반 환풍기(중형)",
+        "model": "플렉스댐퍼 (C2-100LFMD)",
+        "features": "저소음, 고기밀 모터댐퍼, IPX2등급 방수",
+        "cost": "",
+        "defaultPriceText": "120,000원",
+        "detailLink": "",
+        "remarks": "첫째이미지(C2-100LFMD)"
+    },
+    {
+        "id": "p_zeroc",
+        "name": "일반 환풍기(대형)",
+        "model": "제로크 MD (HV3-80-X(MD-N))",
+        "features": "정풍량 인증, 저소음, 고기밀 모터댐퍼, 2-WAY 확장배기",
+        "cost": "",
+        "defaultPriceText": "140,000원",
+        "detailLink": "",
+        "remarks": "둘째이미지(MD-N)"
     }
-];
-
-const defaultMainProducts = [
-    { id: 'p_tioram', customText: '' },
-    { id: 'p_basic', customText: '' }
-];
-
-const defaultAddonProducts = [
-    { id: 'p_terra', customText: '' },
-    { id: 'p_hurricane', customText: '' },
-    { id: 'p_sink', customText: '' }
 ];
 
 const eventsData = [
@@ -69,35 +68,30 @@ const eventsData = [
         "priceText": "복합환풍기 티오람미니 세일 195,000원",
         "periodText": "2026. 07/15(수) ~ 07/216(화)",
         "status": "active",
-        "mainProducts": JSON.parse(JSON.stringify(defaultMainProducts)),
-        "addonProducts": JSON.parse(JSON.stringify(defaultAddonProducts))
+        "mainProducts": [
+            { "id": "p_tioram", "customText": "" },
+            { "id": "p_flex", "customText": "" }
+        ],
+        "addonProducts": [
+            { "id": "p_terra", "customText": "" },
+            { "id": "p_hurricane", "customText": "" },
+            { "id": "p_sink", "customText": "" }
+        ]
     },
     {
-        "id": "ev_1783340698960",
-        "title": "전원주택 빌라 핫썸머",
-        "priceText": "하츠 복합환풍기 티오람미니 파격세일 195,000원",
-        "periodText": "접수기간: 2026. 07. 07(화) ~ 07. 20(월)",
-        "status": "active",
-        "mainProducts": JSON.parse(JSON.stringify(defaultMainProducts)),
-        "addonProducts": JSON.parse(JSON.stringify(defaultAddonProducts))
-    },
-    {
-        "id": "ev_1783340698959",
-        "title": "내포 LH스타힐스 핫썸머",
-        "priceText": "하츠 복합환풍기 파격세일 195,000원",
-        "periodText": "접수기간: 2026. 07. 06(월) ~ 07. 20(월)",
-        "status": "active",
-        "mainProducts": JSON.parse(JSON.stringify(defaultMainProducts)),
-        "addonProducts": JSON.parse(JSON.stringify(defaultAddonProducts))
-    },
-    {
-        "id": "ev_lotte",
-        "title": "내포 롯데캐슬 핫썸머",
-        "priceText": "하츠 복합환풍기 티오람미니 파격세일 195,000원",
-        "periodText": "접수기간: 2026. 06. 30(화) ~ 07. 06(월)",
+        "id": "ev_eg1",
+        "title": "내포 이지더원 1차 핫썸머 2026.6",
+        "periodText": "~6/29",
         "status": "ended",
-        "mainProducts": JSON.parse(JSON.stringify(defaultMainProducts)),
-        "addonProducts": JSON.parse(JSON.stringify(defaultAddonProducts))
+        "mainProducts": [
+            { "id": "p_tioram", "customText": "" },
+            { "id": "p_flex", "customText": "" }
+        ],
+        "addonProducts": [
+            { "id": "p_terra", "customText": "" },
+            { "id": "p_hurricane", "customText": "" },
+            { "id": "p_sink", "customText": "" }
+        ]
     },
     {
         "id": "ev_moa",
@@ -105,23 +99,60 @@ const eventsData = [
         "priceText": "하츠 복합환풍기 티오람미니 파격세일 195,000원",
         "periodText": "접수기간: 2026. 06. 30(화) ~ 07. 06(월)",
         "status": "active",
-        "mainProducts": JSON.parse(JSON.stringify(defaultMainProducts)),
-        "addonProducts": JSON.parse(JSON.stringify(defaultAddonProducts))
+        "mainProducts": [
+            { "id": "p_tioram", "customText": "" },
+            { "id": "p_zeroc", "customText": "" }
+        ],
+        "addonProducts": [
+            { "id": "p_terra", "customText": "" },
+            { "id": "p_hurricane", "customText": "" },
+            { "id": "p_sink", "customText": "" }
+        ]
     },
     {
-        "id": "ev_eg1",
-        "title": "내포 이지더원 1차 핫썸머 2026.6",
-        "periodText": "~6/29",
-        "status": "ended",
-        "mainProducts": JSON.parse(JSON.stringify(defaultMainProducts)),
-        "addonProducts": JSON.parse(JSON.stringify(defaultAddonProducts))
+        "id": "ev_1783340698959",
+        "title": "내포 LH스타힐스 핫썸머",
+        "priceText": "하츠 복합환풍기 파격세일 195,000원",
+        "periodText": "접수기간: 2026. 07. 06(월) ~ 07. 20(월)",
+        "status": "active",
+        "mainProducts": [
+            { "id": "p_tioram", "customText": "" },
+            { "id": "p_zeroc", "customText": "" }
+        ],
+        "addonProducts": [
+            { "id": "p_terra", "customText": "" },
+            { "id": "p_hurricane", "customText": "" },
+            { "id": "p_sink", "customText": "" }
+        ]
     },
     {
-        "id": "ev_eg2",
-        "title": "내포 이지더원 2차 핫썸머",
-        "periodText": "~6/30",
+        "id": "ev_1783340698960",
+        "title": "전원주택 빌라 핫썸머",
+        "priceText": "하츠 복합환풍기 티오람미니 파격세일 195,000원",
+        "periodText": "접수기간: 2026. 07. 07(화) ~ 07. 20(월)",
+        "status": "active",
+        "mainProducts": [
+            { "id": "p_tioram", "customText": "" }
+        ],
+        "addonProducts": [
+            { "id": "p_terra", "customText": "" },
+            { "id": "p_hurricane", "customText": "" },
+            { "id": "p_sink", "customText": "" }
+        ]
+    },
+    {
+        "id": "ev_lotte",
+        "title": "내포 롯데캐슬 핫썸머",
+        "priceText": "하츠 복합환풍기 티오람미니 파격세일 195,000원",
+        "periodText": "접수기간: 2026. 06. 30(화) ~ 07. 06(월)",
         "status": "ended",
-        "mainProducts": JSON.parse(JSON.stringify(defaultMainProducts)),
-        "addonProducts": JSON.parse(JSON.stringify(defaultAddonProducts))
+        "mainProducts": [
+            { "id": "p_tioram", "customText": "" }
+        ],
+        "addonProducts": [
+            { "id": "p_terra", "customText": "" },
+            { "id": "p_hurricane", "customText": "" },
+            { "id": "p_sink", "customText": "" }
+        ]
     }
 ];
